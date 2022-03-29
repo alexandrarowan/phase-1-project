b03608dd3d64360c6f7b3283c2e89dbcc0d7ff6e
Darby Behnken <dtbehnken67@gmail.com>
2022-03-29 13:15:35 -0600

    css updated

--- index.js
+++ index.js
@@ -19,11 +19,14 @@ const coinBuild = (coinObj) => {
     console.log(coinObj)
     const currentConversion = document.querySelector('#Current-Conversion')
     const dollar = document.createElement('h3')
-    dollar.textContent = `USD ${coinObj.USD.rate} Conversion Rate`
+    dollar.id = "us-dollar"
+    dollar.textContent = `USD ${coinObj.USD.rate}`
     const GBP = document.createElement('h3')
-    GBP.textContent = `GBP ${coinObj.GBP.rate} Conversion Rate`
+    GBP.id = "brittish-pound"
+    GBP.textContent = `GBP ${coinObj.GBP.rate}`
     const EUR = document.createElement('h3')
-    EUR.textContent = `EUR ${coinObj.EUR.rate} Conversion Rate`
+    EUR.id = "euro"
+    EUR.textContent = `EUR ${coinObj.EUR.rate}`
     
     currentConversion.append(dollar, GBP, EUR)
     const rate = coinObj.USD.rate_float
