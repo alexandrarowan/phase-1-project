2b094f90547fd3f712d1030a26f3a8dd9caf1293
Alexandra Davis <alex.davis806@gmail.com>
2022-03-29 09:34:36 -0700

    added second event listener

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const fetchBitURL = () => {
     fetch(bitCoinURL)
     .then(resp => resp.json())
     .then(data => {coinBuild(data.bpi)
-      console.log(typeof data.bpi.USD.rate)
     }
     )}
 fetchBitURL()
+
+const currentBalance = document.querySelector('#current-balance')
+const dollarBalance = document.createElement('h3')
+const bitcoinBalance = document.createElement('h3')
+
 const coinBuild = (coinObj) => {
     console.log(coinObj)
     const currentConversion = document.querySelector('#Current-Conversion')
@@ -22,29 +26,45 @@ const coinBuild = (coinObj) => {
     EUR.textContent = `EUR ${coinObj.EUR.rate} Conversion Rate`
     
     currentConversion.append(dollar, GBP, EUR)
+    const rate = coinObj.USD.rate_float
 
     const currentPriceForm = document.querySelector('#current-price')
     currentPriceForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    // console.log(e.target)
     const dollarInput = document.querySelector('#dollar-input').value
-    console.log(typeof dollarInput)
     if (isNaN(dollarInput) === true) {
         alert('Error! You must enter a number!')
     } else {
-        const rate = Math.abs(coinObj.USD.rate)
-        console.log(rate)
-        // console.log('hi')
+        const coins = parseFloat(dollarInput) / rate
+        bitcoinBalance.textContent = `${coins} coins`
+        currentBalance.append(bitcoinBalance)
+
     }
-    
-    // const newObj = {
-        
-    // }
-    // function doMath(input, rate) {
-    //     return input / rate
-    // }
+    currentPriceForm.reset();
 })
+    const priceForm = document.querySelector('#price-form')
+    priceForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        // e.stopPropagation();
+        const transferAmount = document.querySelector('#transfer-amount').value
+        // console.log(transferAmount)
+        if (isNaN(transferAmount) === true) {
+            alert('Error! You must enter a number!')
+        } else {
+            const dollars = parseFloat(transferAmount) * rate
+            dollarBalance.textContent = `$ ${dollars} USD` 
+            currentBalance.append(dollarBalance) 
+            
+        }
+        priceForm.reset()
+    })
 }
+
+const currentBal = (input) => {
+    
+}
+
+
 
 
 
